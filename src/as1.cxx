178c2d8262a182638b711392c04ae5ba0a80836a
Daniel Stålhammar <danst@cosmos-pkf.int>
2025-10-22 18:35:00 +0200

    New trye. Hopfully better than previous atempt

--- src/as1.cxx
+++ src/as1.cxx
@@ -4,12 +4,28 @@ namespace homework {
 
 void printHello() { std::cout << "Hello, World!" << std::endl; }
 
-void AddOneRef(int &x) { return; }
+void AddOneRef(int &x) { x+=1; }
 
-bool isOdd(int x) { return false; }
+bool isOdd(int x) { if (x%2=0) { 
+    return false;} }
 
-int floatToInt(float x) { return 0; }
+int floatToInt(float x) { 
+    int x_int=static_cast<int>(std::round(x));   // If I understud corectly it was so it should be done to convert a float number to an int number
+    return 0; 
 
-int factorial(int n) { return 0; }
+
+
+}
+
+int factorial(int n) { 
+    l=1
+    (if n<0) {
+    return -1};
+
+    (if n==1){return l};
+    (if n > 1)
+        while n >1
+        {l = l*n; (n-=1);};
+    return l }                         // The best I could findout this time
 
 }; // namespace homework
