70f37b6c7c838f862a401256a7f85709b3d9909a
Daniel Stålhammar <danst@cosmos-pkf.int>
2025-10-27 13:03:55 +0100

    A trye to solve the homework

--- include/as3.hpp
+++ include/as3.hpp
@@ -10,9 +10,36 @@ namespace homework {
 // (a) TO DO: Implement your type (Hint: enum class). Name it "Color" and add three colors:
 // red, green, yellow
 
+class colors(fruit){
+    public
+    red ="red";
+    green ="green";
+    yellow = "yellow";
+
+    if fruit == "aple";
+        return red+fruit
+    else if fruit == "pear";
+        return green+fruit
+    else if fruit == "banan";
+        return yellow+fruit;
+    }
+
+class fruits():public colors{
+    fruit == get_name();
+    get_color == colors(fruit);
+    return get_color;
+       
+}
+
 // (b) TO DO: Implement a class called "Fruit" that has a constructor taking a
 // string and a "Color" and two methods: "getName" and "getColor" Also implement
 // a pure virtual method "getTaste" that returns a string
+
+class apple():public fruits{
+    taste=get_taste();
+    Color =colors(taste);
+    cout <<Color+" taste good"<<endl;
+}
 
 // (c) TO DO: Implement a class called "Apple" that inherits from "Fruit"
 // implement the constructor and the "getTaste" method
